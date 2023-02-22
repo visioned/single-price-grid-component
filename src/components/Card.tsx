f8fc13a79552dd9af37a906ef664d2e6b2070778
visioned <ajkg11@gmail.com>
2023-02-21 17:27:04 -0800

    rounded

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,8 +9,8 @@ export const Card = () => {
     "New videos every week",
   ];
   return (
-    <div className="flex flex-col shadow-2xl lg:rounded-bl-[8px] lg:rounded-br-[8px]">
-      <div className="lg:h-[216px] lg:w-[635px] lg:rounded-tl-[8px] lg:rounded-tr-[8px] h-[267px] w-[311px] bg-[#fff] py-[24px] px-[25px] lg:py-[40px] lg:px-[40px]">
+    <div className="flex flex-col shadow-2xl lg:rounded-bl-[8px] lg:rounded-br-[8px] rounded-[8px]">
+      <div className="lg:h-[216px] lg:w-[635px] lg:rounded-tl-[8px] lg:rounded-tr-[8px] h-[267px] w-[311px] bg-[#fff] py-[24px] px-[25px] lg:py-[40px] lg:px-[40px] rounded-t-[6px]">
         <h2 className="text-[#2ab2af] font-bold text-xl tracking-tight lg:text-2xl">
           Join our community
         </h2>
@@ -25,7 +25,7 @@ export const Card = () => {
       </div>
 
       <div className="lg:flex lg:flex-row">
-        <div className="lg:h-[259px] lg:w-[317.5px] h-[228px] w-[311px] bg-[#2BB3B1] lg:py-[40px] py-[20px] lg:px-[40px] px-[25px] lg:rounded-bl-[8px]">
+        <div className="lg:h-[259px] lg:w-[317.5px] h-[228px] w-[311px] bg-[#2BB3B1] lg:py-[40px] py-[20px] lg:px-[40px] px-[25px] lg:rounded-bl-[8px] ">
           <h2 className="text-lg font-bold">Monthly Subscription</h2>
           <h3 className="mt-[8px] font-bold text-[32px]">
             $29 &nbsp;
@@ -34,12 +34,12 @@ export const Card = () => {
             </span>
           </h3>
           <p className="mt-[1px]">Full access for less than $1 a day</p>
-          <button className="h-[48px] w-[263px] lg:w-[237px] bg-[#c0df34] text-[#fff] rounded-[4px] mt-[26px] shadow-lg">
+          <button className="h-[48px] w-[263px] lg:w-[237px] font-bold bg-[#c0df34] text-[#fff] rounded-[4px] mt-[26px] shadow-lg">
             Sign Up
           </button>
         </div>
 
-        <div className="lg:h-[259px] lg:w-[317.5px] h-[227px] w-[311px] bg-[#4ABEBD] py-[20px] px-[25px] lg:py-[40px] lg:px-[40px] lg:rounded-br-[8px]">
+        <div className="lg:h-[259px] lg:w-[317.5px] h-[227px] w-[311px] bg-[#4ABEBD] py-[20px] px-[25px] lg:py-[40px] lg:px-[40px] lg:rounded-br-[8px] rounded-b-[6px]">
           <h2 className="font-bold text-lg">Why Us</h2>
           <ul className="mt-4">
             {why.map((item) => (
